a265ba37baaa3b8c792f655a88f5ee27e31dd4e4
Laura Neves <lauratneves@gmail.com>
2022-11-08 22:08:21 -0300

    "Criando DTO para seleção de período de transação a ser filtrado"

---
+++ src/main/java/com/elasbancam/dtos/DataTransacaoDto.java
@@ -0,0 +1,17 @@
+package com.elasbancam.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class DataTransacaoDto {
+    @NotBlank(message = "Campo data inicial deve ser preenchido")
+    private LocalDateTime dataInicial;
+
+    @NotBlank(message = "Campo data final deve ser preenchido")
+    private LocalDateTime dataFinal;
+}

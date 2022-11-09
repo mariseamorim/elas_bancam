71b37c75530a572925a8d4d0187a6a860d905b95
Laura Neves <lauratneves@gmail.com>
2022-11-09 20:07:51 -0300

    "Eliminando getters e setters dos campos de ids"

--- src/main/java/com/elasbancam/models/Conta.java
+++ src/main/java/com/elasbancam/models/Conta.java
@@ -8,29 +8,38 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "conta")
 public class Conta {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Getter
     private UUID id;
 
+    @Getter
+    @Setter
     @Column(length = 20, nullable = false, updatable = false)
     private int numeroConta;
 
+    @Getter
+    @Setter
     @Column(length = 10, nullable = false, updatable = false)
     private int agencia;
 
+    @Getter
+    @Setter
     @Enumerated(EnumType.ORDINAL)
     @Column(nullable = false)
     private TipoOperacao operacao;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
     private BigDecimal saldo;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
     private boolean status;
 }
--- src/main/java/com/elasbancam/models/Transacao.java
+++ src/main/java/com/elasbancam/models/Transacao.java
@@ -10,34 +10,45 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "transacao")
 public class Transacao {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Getter
     private UUID id;
 
+    @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "conta_origem_id", referencedColumnName = "id")
     private Conta conta_origem_id;
 
+    @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "conta_destino_id", referencedColumnName = "id")
     private Conta conta_destino_id;
 
+    @Getter
+    @Setter
     @Enumerated(EnumType.ORDINAL)
     @Column(nullable = false)
     private TipoTransacao tipo_trasacao;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
     @CreationTimestamp
     private LocalDateTime data;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
     private BigDecimal valor;
 
+    @Getter
+    @Setter
     @Column()
     private String descricao;
 }

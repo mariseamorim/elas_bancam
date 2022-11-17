632ee445455776cef839844165bd861fbafd3a7c
Ana Flavia <anaflavia.199@gmail.com>
2022-11-16 22:03:38 -0300

    remove setCpf

--- src/main/java/com/elasbancam/models/PessoaFisica.java
+++ src/main/java/com/elasbancam/models/PessoaFisica.java
@@ -1,17 +1,14 @@
 package com.elasbancam.models;
 
 import com.elasbancam.enums.Genero;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
-@Data
 @Getter
 @Setter
 @Entity
@@ -20,10 +17,6 @@ public class PessoaFisica extends Pessoa{
     @Column(length = 20, nullable = false, unique = true)
     @CPF
     private String cpf;
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 
     @Column(length = 20, nullable = false, unique = true)
     private String rg;
@@ -34,7 +27,6 @@ public class PessoaFisica extends Pessoa{
 
     @Column(nullable = false)
     private String nome_mae;
-    @NotBlank
 
     @Column(length = 20, nullable = false)
     @Enumerated(EnumType.STRING)
